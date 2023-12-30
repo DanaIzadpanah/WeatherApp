f439d183de863f7458ecf4d98ca263edda90ec9b
Dana Izadpanah <danaizad8624@gmail.com>
2023-12-30 14:06:04 -0600

    Setting up the skeleton

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+const api = {
+  key: "4f8e795dcd6dbf7b9f5276bff095ffc1",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className='search-box'>
+          <input
+            type='text'
+            className='search-bar'
+            placeholder='Search...'
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        <div>
+            <div className='location-box'>
+              <div className='location'>New York City, US</div>
+              <div className='date'>{dateBuilder(new Date())}</div>
+
+            </div>
+        </div>
+      </main>
     </div>
   );
 }

473ab224aea826eb8c637291f2d75d9c111ba049
Dana Izadpanah <danaizad8624@gmail.com>
2023-12-30 20:11:21 -0600

    Commenting half of the code

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,3 +1,4 @@
+// Using the useState hook 
 import React, { useState } from 'react';
 const api = {
   key: "4f8e795dcd6dbf7b9f5276bff095ffc1",
@@ -5,9 +6,18 @@ const api = {
 }
 
 function App() {
+
+  /**
+   * query will store the current value of the search input 
+   * weather will store the weather data fetched from the API 
+   */
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
+
+  /** 
+   * This block defines the search function, which is called when a key is pressed. If the 'Enter' key is pressed, it uses fetch to make an API request using the query state for the city and the API key. When the data is returned, it updates the weather state with the result and clears the query state.
+   */
   const search = evt => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`)
@@ -20,21 +30,23 @@ function App() {
     }
   }
 
+
+  // The dateBuilder function creates a human-readable date string from a Date object. It's used to display the current date in the app.
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
-
     return `${day} ${date} ${month} ${year}`
   }
 
   return (
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
+
+
         <div className="search-box">
           <input 
             type="text"
@@ -45,7 +57,11 @@ function App() {
             onKeyPress={search}
           />
         </div>
+
+
         {(typeof weather.main != "undefined") ? (
+
+          
         <div>
           <div className="location-box">
             <div className="location">{weather.name}, {weather.sys.country}</div>
